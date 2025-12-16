513434788ea2e9ad2b8b644126870582053598d2
mahendiran1202 <mahemaahi07@gmail.com>
2025-12-16 09:56:19 +0530

    Create fulladder_tb.v for testing full adder
    Added testbench for full adder functionality.

---
+++ DAY_2 /FULL_ADDER/Full_adder_tb.v
@@ -0,0 +1,21 @@
+//TB
+
+module fulladder_tb;
+reg a,b,cin;
+wire sum,carry;
+fulladder dut(.a(a),.b(b),.cin(cin),.sum(sum),.carry(carry));
+initial begin
+$monitor("a=%b | b=%b | cin=%b | sum=%b | carry=%b |time=%t",a,b,cin,sum,carry,$time);
+$dumpfile("fulladder.vcd");
+$dumpvars();
+a=0;b=0;cin=0;#10;
+a=0;b=0;cin=1;#10;
+a=0;b=1;cin=0;#10;
+a=0;b=1;cin=1;#10;
+a=1;b=0;cin=0;#10;
+a=1;b=0;cin=1;#10;
+a=1;b=1;cin=0;#10;
+a=1;b=1;cin=1;#10;
+$finish;
+end 
+endmodule
